2d6866117232310998f17d4c1f50f98a78221158
AndreeFarro <andreefarro10@hotmail.com>
2022-08-18 18:51:26 -0500

    valid forms

--- app/src/main/java/com/uns/taxiflores/MainActivity.kt
+++ app/src/main/java/com/uns/taxiflores/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.WindowManager
+import android.widget.Toast
 import com.uns.taxiflores.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,29 @@ class MainActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
         binding.btnRegister.setOnClickListener { goToRegister() }
+        binding.btnLogin.setOnClickListener { login() }
+
+    }
+
+    private fun login(){
+        val email = binding.textFieldEmail.text.toString()
+        val password = binding.textFieldPassword.text.toString()
+
+        if(isValidForm(email,password)){
+            Toast.makeText(this, "form valido", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isValidForm(email:String, password:String) : Boolean{
+        if(email.isEmpty()){
+            Toast.makeText(this, "Ingresar su Correo Electronico", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(password.isEmpty()){
+            Toast.makeText(this, "Ingresar su Contraseña", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 
     private fun goToRegister(){
--- app/src/main/java/com/uns/taxiflores/RegisterActivity.kt
+++ app/src/main/java/com/uns/taxiflores/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.uns.taxiflores
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
 import com.uns.taxiflores.databinding.ActivityMainBinding
 import com.uns.taxiflores.databinding.ActivityRegisterBinding
 
@@ -14,8 +16,63 @@ class RegisterActivity : AppCompatActivity() {
 
         binding= ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         binding.btnGoToLogin.setOnClickListener {goToLogin()}
+
+        binding.btnRegister.setOnClickListener{ register() }
     }
+
+    private fun register(){
+        val name = binding.textFieldName.text.toString()
+        val lastname = binding.textFieldLastName.text.toString()
+        val email = binding.textFieldEmail.text.toString()
+        val phone = binding.textFieldPhone.text.toString()
+        val password = binding.textFieldPassword.text.toString()
+        val confirmPassword = binding.textFieldConfirmPassword.text.toString()
+
+        if(isValidForm(name,lastname, email, phone, password, confirmPassword)){
+            notification("Form valido")
+        }
+
+    }
+
+    private fun isValidForm(
+        name:String,
+        lastname:String,
+        email:String,
+        phone:String,
+        password:String,
+        confirmPassword:String
+    ): Boolean{
+
+        if (
+            name.isEmpty()      ||
+            lastname.isEmpty()  ||
+            email.isEmpty()     ||
+            phone.isEmpty()     ||
+            password.isEmpty()  ||
+            confirmPassword.isEmpty()
+        ){
+            notification("Llene Todos los campos")
+            return false
+        }
+
+        if(password != confirmPassword){
+            notification("Las contraseñas deben coincidir")
+            return false
+        }
+        if (password.length < 6){
+            notification("La contraseña debe ser mayor a 6 carateres")
+            return false
+        }
+
+        return true
+    }
+
+    private fun notification(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     private fun goToLogin(){
         val i = Intent(this, MainActivity::class.java)
         startActivity(i)
