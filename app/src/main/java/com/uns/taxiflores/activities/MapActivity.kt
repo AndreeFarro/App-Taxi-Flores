6618a1da11ea354cad56c0a1128edd98334a69e6
AndreeFarro <andreefarro10@hotmail.com>
2022-08-21 17:09:46 -0500

    integracion de mapa - google maps

--- app/src/main/java/com/uns/taxiflores/activities/MapActivity.kt
+++ app/src/main/java/com/uns/taxiflores/activities/MapActivity.kt
@@ -2,18 +2,30 @@ package com.uns.taxiflores.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.uns.taxiflores.R
 import com.uns.taxiflores.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(),OnMapReadyCallback {
 
     private lateinit var binding : ActivityMapBinding
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
     }
 }
